52d701ed7e85a026185e16130067ec722b4dadfe
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2019-12-30 15:23:00 -0300

    remove external font-awesome link and add it locally

--- src/html.js
+++ src/html.js
@@ -19,10 +19,6 @@ export default function HTML({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-        />
         {headComponents}
       </head>
       <body {...bodyAttributes}>
